18c6b7b71de4f4fa363a73bbb92bd2f0105dfc47
Arkodeb Maity <maityarkodeb@gmail.com>
2017-06-24 18:52:19 -0400

    fix master

--- assets/js/bros.js
+++ assets/js/bros.js
@@ -8,4 +8,48 @@ $(document).ready(function() {
 		let location = $('#location').val();
 		console.log(location)
 	});
+
+	var yelpurl  = "https://api.yelp.com/v3/businesses/search?term=frat&location=";
+	var location = "Atlanta"
+	// inputlocation.replace(",", "%2C")
+	var totalurl = yelpurl + location
+	var secretpass = "jE6PgDRPzcRPX7bY0i5huNjzGcAU84H1b53LCNziU20kojs5PS4WM4ZWJttRbEkaZ_aLy3leic6lC0tPy6VBpj-cwRO5ZJbrvsraR_r5N3OeGKdeJj16thk4TQ5LWXYx"
+
+
+	function retrieveInfo() {
+	 $.ajax({
+	      
+	      xhrFields: { withCredentials: true},
+	      headers: {'Authorization': 'Bearer ' + secretpass},
+	      url: yelpurl + location ,
+	      type: 'GET',
+	      
+	      success: function(data) {
+	        var lengtharray = data.busineses.length
+	        var arr = []
+
+	        for (var i =o; i < lengtharray; i++) {
+	            arr.push({
+	            name : data.businesses[i].name,
+	            lat : data.businesses[i].coordinates.latitude,
+	            long : data.businesses[i].coordinates.longitude
+	            })
+	        }
+	      }
+	})
+	}
+
+	retrieveInfo()
+
+	 /* xhrFields: {
+	        withCredentials: true
+	    },
+	    headers: {
+	        'Authorization': 'Basic ' + btoa('myuser:mypswd')
+	    },
+	    url: "https://www.example.org/protected-data.php"
+	}**/
+
+
+	
 });
