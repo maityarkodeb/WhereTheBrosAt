312cb474fbd3a24f366d38e019b94bcac078ab16
Matthew <discarnate@gmail.com>
2017-06-25 16:13:11 -0400

    added google maps srcipt src tag in index; added googlemaps logic in bros.js

--- public/assets/js/bros.js
+++ public/assets/js/bros.js
@@ -8,6 +8,34 @@ $(document).ready(function() {
 		let location = $('#location').val();
 		console.log(location)
 		retrieveInfo(location)
+
+
+	/*
+
+	 var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: location,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(arr[i][1], arr[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(arr[i][0]), arr[i][3];
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+	**/
 	});
 
 	function retrieveInfo(location) {
@@ -16,10 +44,10 @@ $(document).ready(function() {
 	      url: ("/" + location),
 	      
 	      success: function(data) {
-	        var lengtharray = data.businesses.length
-	        var arr = []
+	        let lengtharray = data.businesses.length
+	        let arr = []
 
-	        for (var i = 0; i < lengtharray; i++) {
+	        for (let i = 0; i < lengtharray; i++) {
 	            arr[i] = [data.businesses[i].name, 
 	            data.businesses[i].coordinates.latitude, 
 	            data.businesses[i].coordinates.longitude,
